59db1a15721f3f2a9b44f14a8e44bbf3b247fd23
Earl Hur <earlhur88@gmail.com>
2019-03-31 23:12:32 -0500

    New!

---
+++ app.R
@@ -0,0 +1,67 @@
+library(shiny)
+library(tidyr)
+library(tibble)
+library(rvest)
+
+pet_fun <- function(x) {
+  tibble(
+    name = xml_node(x, "name") %>% xml_text(),
+    breed = xml_nodes(x, "breed") %>% xml_text() %>% paste(collapse = ", "),
+    age = xml_node(x, "age") %>% xml_text(),
+    sex = xml_node(x, "sex") %>% xml_text(),
+    id = xml_node(x, "id") %>% xml_text(),
+    shelterID = xml_node(x, "shelterId") %>% xml_text()
+  )
+}
+
+ui <- fluidPage(
+  
+  titlePanel("Let's find your pet!"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(inputId = "location", 50014,
+                   label = "Your ZIP code"),
+      radioButtons(inputId = "animal",
+                  label = "Cat or Dog?:",
+                  choices = c("cat", "dog"))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      h1("Pets in your location"),
+      DT::dataTableOutput("table")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$table <- DT::renderDataTable(
+    
+      DT::datatable({
+        selectedzip <- input$location
+        selectedpet <- input$animal
+        
+        key <- "key=7fdd14996c2133c74184f71e087ddf19"
+        base_url <- "http://api.petfinder.com/"
+        method <- "pet.find"
+        
+        query <- paste0("animal=", selectedpet, "&", "location=", selectedzip)
+        url <- sprintf("%s%s?%s&%s", base_url, method, key, query)
+        pets_list <- read_xml(url) %>% xml_nodes("pet")
+        
+        pet_df <- pets_list %>% purrr::map_df(pet_fun)
+        
+        pet_df
+        
+        }, options = list(pageLength = 20, dom = "tip"))
+      
+  )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
