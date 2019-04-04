4fcf85ac9e903e20b77afff85f3a74504b918fef
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-04 17:20:11 -0500

    Package should build now

--- R/extract_data.R
+++ R/extract_data.R
@@ -1,30 +1,7 @@
-library(tidyverse)
-
-# Possible searches are documented here:
-# # https://www.petfinder.com/developers/v2/docs/
-
-
-# This is how you complete a search. You need an access token to search
-# # (see authorization.R). Now we need to figure out how to parse the search
-# # results so we can display them for a user, especially pictures.
-
-
-# This search gets the first 20 animals from the front page
-search_results <- httr::GET(url = "https://api.petfinder.com/v2/animals",
-                    httr::add_headers(Authorization = paste("Bearer", token)))
-# content() gives us the actual content of the search
-raw_info <- httr::content(search_results)
-animal_info <- httr::content(search_results)$animals
-
-
-# Helper function for blank fields
 check_null <- function(x) {
   ifelse(!is.null(x), x, NA)
 }
 
-# We can manually extract the information (I started to write that here),
-# # but it gets really tedious, and it would be better to automatically
-# # detect the fields and populate them, instead of defining them all.
 animals_to_df <- function(x) {
   tibble(id = x$id, organization_id = x$organization_id, url = x$url,
          type = x$type, species = x$species,
@@ -38,7 +15,20 @@ animals_to_df <- function(x) {
          gender = x$gender, name = x$name)
 }
 
-x <- animal_info[[1]]
-names(x)
-names(unlist(x))
-animal_df <- animal_info %>% purrr::map_df(animals_to_df)
+petfindr_search <- function(token, query) {
+  library(httr)
+  library(magrittr)
+  library(tidyverse)
+  base <- "https://api.petfinder.com/v2/"
+  query <- "animals"
+  url <- paste0(base, query)
+  search_results <- GET(url = url, 
+                        add_headers(Authorization = paste("Bearer", token)))
+  
+  
+  animal_df <- content(search_results)$animals %>% purrr::map_df(animals_to_df)
+  return(animal_df)
+  
+}
+
+# petfindr_search(token)
