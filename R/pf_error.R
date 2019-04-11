1dcb0c8ffb20d3c2c345260d4d957a031a37a7e5
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-10 23:16:32 -0500

    Add function to parse error codes

---
+++ R/pf_error.R
@@ -0,0 +1,11 @@
+pf_error <- function(error_code) {
+  message <- switch(as.character(error_code),
+                    "401" = "Access was denied due to invalid credentials. This could be an invalid API key/secret combination, missing access token, or expired access token.",
+                    "403" = "Access denied due to insufficient access.",
+                    "404"= "The requested resource was not found.",
+                    "500" = "The request ran into an unexpected error. If the problem persists, please contact support at https://www.petfinder.com/developers/support/.",
+                    "00001" = "The request has missing parameters.",
+                    "00002" = "Your request contains invalid parameters.")
+  return(message)
+}
+
