e6a8edb9c78701705dfc93680e0b83299c76576b
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-10 23:36:23 -0500

    Update available codes to include general 400 and 200 OK

--- R/pf_error.R
+++ R/pf_error.R
@@ -1,5 +1,7 @@
-pf_error <- function(error_code) {
-  message <- switch(as.character(error_code),
+pf_error <- function(status_code) {
+  message <- switch(as.character(status_code),
+                    "200" = "OK",
+                    "400" = "The request is missing parameters or contains invalid parameters.",
                     "401" = "Access was denied due to invalid credentials. This could be an invalid API key/secret combination, missing access token, or expired access token.",
                     "403" = "Access denied due to insufficient access.",
                     "404"= "The requested resource was not found.",
