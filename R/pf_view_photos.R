fe9fc3d47df4d84329af8107b32806d55e4c3bda
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-18 12:11:36 -0500

    Shorten redundant "if" statements

--- R/pf_view_photos.R
+++ R/pf_view_photos.R
@@ -7,63 +7,80 @@
 #' @export
 #'
 #' @examples 
-#` animals_of_interest<-pf_find_pets(token, location = 50014, distance = 100, type = "dog", 
-#` breed = "corgi", gender = c("male", "female"), limit=100, page="all", sort = "distance")
-#` pf_photo_view(search_result= animals_of_interest, size= "medium")
-
-
+#' \dontrun{
+#' corgis <- pf_find_pets(token, location = 50014, distance = 100, type = "dog",
+#'     breed = "corgi", gender = c("male", "female"), limit = 100, page = "all", 
+#'     sort = "distance")
+#' pf_photo_view(search_result = corgis, size = "medium")
+#' }
 
 pf_photo_view<- function(search_result, size=c("small", "medium", 
                                                "large","full")){
-  assertthat::assert_that(is.data.frame( search_result))
+  assertthat::assert_that(is.data.frame(search_result))
   assertthat::not_empty(search_result)
-  size<- match.arg(size )
-  if (size== "small"){animal_photos<- search_result %>%
-    select( tidyselect::vars_select(names(search_result),
-                                    starts_with("photos.small", 
-                                                ignore.case = TRUE))) }
+  size <- match.arg(size)
   
-  if (size== "medium"){animal_photos<- search_result %>%
+  # Amin: I think these lines do what your next four "if" statements do.
+  animal_photos <- search_result %>%
     select(tidyselect::vars_select(names(search_result),
-                                   starts_with("photos.medium", 
-                                               ignore.case = TRUE)))} 
+                                   starts_with(paste0("photos.", size)),
+                                   ignore.case = T))
   
-  if (size== "large"){animal_photos<- search_result %>%
-    select(tidyselect::vars_select(names(search_result),
-                                   starts_with("photos.large", 
-                                               ignore.case = TRUE)))} 
+  # if (size== "small"){animal_photos<- search_result %>%
+  #   select( tidyselect::vars_select(names(search_result),
+  #                                   starts_with("photos.small", 
+  #                                               ignore.case = TRUE))) }
+  # 
+  # if (size== "medium"){animal_photos<- search_result %>%
+  #   select(tidyselect::vars_select(names(search_result),
+  #                                  starts_with("photos.medium", 
+  #                                              ignore.case = TRUE)))} 
+  # 
+  # if (size== "large"){animal_photos<- search_result %>%
+  #   select(tidyselect::vars_select(names(search_result),
+  #                                  starts_with("photos.large", 
+  #                                              ignore.case = TRUE)))} 
+  # 
+  # if (size== "full"){animal_photos<- search_result %>%
+  #   select(tidyselect::vars_select(names(search_result),
+  #                                  starts_with("photos.full", 
+  #                                              ignore.case = TRUE)))} 
   
-  if (size== "full"){animal_photos<- search_result %>%
-    select(tidyselect::vars_select(names(search_result),
-                                   starts_with("photos.full", 
-                                               ignore.case = TRUE)))} 
   assertthat::not_empty(animal_photos)
-  animal_photos<-animal_photos %>% lapply(FUN = as.character) %>% flatten()
-  if (size== "small"){
-    
-    photos<-purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
-      str_remove("&width=100") 
-  }
-  if (size== "medium"){
-    
-    photos<- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
-      str_remove("&width=300") 
-  }
-  if (size== "large"){
-    
-    photos<- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
-      str_remove("&width=600") 
-  }
-  if (size== "full"){
-    
-    photos<- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
-      str_remove("&width=600") 
-  }
+  animal_photos<- animal_photos %>% lapply(FUN = as.character) %>% flatten()
   
-  return( photos %>%
-            knitr::include_graphics() %>% magick::image_read() %>%
-            magick::image_scale("400") %>% 
-            magick::image_scale("x400") )
+  # Amin: I think these lines do what your next four "if" statements do.
+  npix <- (as.character(c(100, 300, 600, 600)) %>% 
+             setNames(c("small", "medium", "large", "full")))[size]
+  photos <- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% 
+    na.omit %>%
+    str_remove(paste0("&width=", npix))
+  
+  # if (size== "small"){
+  #   
+  #   photos<-purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
+  #     str_remove("&width=100") 
+  # }
+  # if (size== "medium"){
+  #   
+  #   photos<- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
+  #     str_remove("&width=300") 
+  # }
+  # if (size== "large"){
+  #   
+  #   photos<- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
+  #     str_remove("&width=600") 
+  # }
+  # if (size== "full"){
+  #   
+  #   photos<- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>% na.omit() %>% 
+  #     str_remove("&width=600") 
+  # }
+  
+  return(photos %>%
+           knitr::include_graphics() %>% magick::image_read() %>%
+           magick::image_scale("400") %>% 
+           magick::image_scale("x400"))
 }
 
 
