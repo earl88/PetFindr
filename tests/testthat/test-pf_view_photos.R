ca2b26a23f617d2eecf43ef0a47ebb3668b7c0b1
Amin Shirazi <ashirazi@iastate.edu>
2019-04-20 17:58:44 -0500

    update testthat

--- tests/testthat/test-pf_view_photos.R
+++ tests/testthat/test-pf_view_photos.R
@@ -1,10 +1,11 @@
 context("test-pf_view_photos")
 
 test_that("correct animal dataframe ", {
-  expect_true(is.data.frame(animal_df))
-#  expect_is(animal_df, "data.frame")
-  expect_true(is.character(size))
+#   expect_true(is.data.frame(animal_df= pf_find_pets(token) ))
+#   expect_is(animal_df, "data.frame")
+#   expect_true(is.character(size))
 #  expect_is(size, "character")
+  
   # expect an error due to incorrect search result from pf_find_pet
   expect_error(pf_view_photos(animal_df = puppies, size = 123))
   
