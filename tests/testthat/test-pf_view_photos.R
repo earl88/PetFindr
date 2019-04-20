22040b16dceb659ddf575557310f83542807921e
Amin Shirazi <ashirazi@iastate.edu>
2019-04-20 17:53:54 -0500

    update test function

--- tests/testthat/test-pf_view_photos.R
+++ tests/testthat/test-pf_view_photos.R
@@ -1,8 +1,10 @@
 context("test-pf_view_photos")
 
 test_that("correct animal dataframe ", {
-  expect_is(animal_df, "data.frame")
-  expect_is(size, "character")
+  expect_true(is.data.frame(animal_df))
+#  expect_is(animal_df, "data.frame")
+  expect_true(is.character(size))
+#  expect_is(size, "character")
   # expect an error due to incorrect search result from pf_find_pet
   expect_error(pf_view_photos(animal_df = puppies, size = 123))
   
