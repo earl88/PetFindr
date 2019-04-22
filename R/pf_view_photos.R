e33e6a39541e397381ac2b4a6bd83fc56cbe560e
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-22 10:41:14 -0500

    White space and some rephrasing

--- R/pf_view_photos.R
+++ R/pf_view_photos.R
@@ -1,38 +1,32 @@
-#' Slideshow all available photos of the animals of interest in R
+#' View Photos of Pets
+#' 
+#' This function takes a data frame of searched pets and displays the image in
+#' a slideshow format.
 #'
-#' @param animal_df A data frame of the animals of interest that have been searched for (using pf_find_pet)
-#' @param size The favorite size of the animal photos to be shown 
+#' @param animal_df A data frame of animal search results from pf_find_pets().
+#' @param size The desired size of the animal photos to be shown 
 #'
-#' @return A slideshow of the searched pets
+#' @return A slideshow of animal pictures
 #' @export
 #'
 #' @examples 
 #' \dontrun{
-#'   puppies <- pf_find_pets(token, type = "dog", age = "baby", breed= "corgi" )
+#'   puppies <- pf_find_pets(token, type = "dog", age = "baby", breed = "corgi")
 #'   pf_view_photos(animal_df = puppies, size = "small")
 #'   
-#'   bunnies <- pf_find_pets(token = token, type = "rabbit", age = "baby", limit = 10)
+#'   bunnies <- pf_find_pets(token, type = "rabbit", age = "baby", limit = 10)
 #'   pf_view_photos(animal_df = bunnies, size = "full")
 #' }
-
-
-
-pf_view_photos <-
-  function(animal_df,
-           size = c("small", "medium", "large", "full")) {
+pf_view_photos <- function(animal_df, 
+                           size = c("small", "medium", "large", "full")) {
     assertthat::assert_that(is.data.frame(animal_df))
     assertthat::not_empty(animal_df)
-    assertthat::not_empty(size)
     size <- match.arg(size)
-    
     
     animal_photos <- animal_df %>%
       dplyr::select(tidyselect::vars_select(
         names(animal_df),
-        dplyr::starts_with(paste0("photos.", size),
-                           ignore.case = TRUE)
-      ))
-    
+        dplyr::starts_with(paste0("photos.", size), ignore.case = TRUE)))
     
     assertthat::not_empty(animal_photos)
     animal_photos <- animal_photos %>%
@@ -41,14 +35,14 @@ pf_view_photos <-
     
     npix <- (as.character(c(100, 300, 600, 600)) %>%
                stats::setNames(c("small", "medium", "large", "full")))[size]
-    photos <-
-      purrr::map_chr(animal_photos, magrittr::extract2, 1) %>%
+    
+    photos <- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>%
       stats::na.omit() %>%
       stringr::str_remove(paste0("&width=", npix))
     
-    return(
-      photos %>%
-        knitr::include_graphics() %>% magick::image_read() %>%
-        magick::image_scale("x400")
+    return(photos %>%
+             knitr::include_graphics() %>%
+             magick::image_read() %>%
+             magick::image_scale("x400")
     )
   }
