d4f46ff3104db3f669e55b72535c2b9ef144602e
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-22 18:10:16 -0500

    test travis

--- tests/testthat/test-authorization.R
+++ tests/testthat/test-authorization.R
@@ -3,3 +3,10 @@ context("test-authorization")
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("can use key and secret", {
+  expect_type(Sys.getenv("pf_key"), "character")
+  # expect_success(pf_accesstoken(Sys.getenv(x = "pf_key"), 
+                                # Sys.getenv(x = "pf_secret")))
+})
+  
