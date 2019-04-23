b1e0da02156a7d1b690227f4e37b15cfc2cd62e4
jesslk <35576403+jesslk@users.noreply.github.com>
2019-04-22 22:49:22 -0500

    following Dean Attali's instruction

---
+++ inst/shiny-examples/myapp/ui.R
@@ -0,0 +1,25 @@
+
+library(shiny)
+library(zipcode)
+library(tidyr)
+library(leaflet)
+library(PetFindr)
+library(tidyverse)
+
+ui <- fluidPage(
+  
+  titlePanel("Find Animal Shelters Near You!"),
+  
+  sidebarPanel(
+    textInput("Zipcode", "Enter a 5-digit U.S. ZIP (postal) code to see the shelters:", value="50010"),
+    selectInput("Distance", "Input miles from the zip code:", choices = seq(50, 500, by=50), selected = "100"),
+    submitButton("Submit"),
+    width = 4),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Animal Shelters", leafletOutput("plot"),
+               width = 8)
+    )
+  )
+)
