7595e626e9a086b3e6e146343d740be905f07dab
Earl Hur <earlhur88@gmail.com>
2019-04-23 12:28:38 -0500

    small change

--- R/runExample.R
+++ R/runExample.R
@@ -2,7 +2,9 @@
 runExample <- function() {
   appDir <- system.file("shiny-examples", "myapp", package = "PetFindr")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `PetFindr`.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing `PetFindr`.",
+         call. = FALSE
+    )
   }
   
   shiny::runApp(appDir, display.mode = "normal")
