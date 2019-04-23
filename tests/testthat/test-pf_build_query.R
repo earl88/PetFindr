eba9f2e70e999a6d0a11d96a62d75a92adf4939f
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-23 16:07:15 -0500

    Update tests

--- tests/testthat/test-pf_build_query.R
+++ tests/testthat/test-pf_build_query.R
@@ -3,5 +3,5 @@ context("test-pf_build_query")
 test_that("building query works", {
   expect_equivalent(pf_build_query(list(limit = 100)), "limit=100")
   expect_equivalent(pf_build_query(list(type = "dog", location = "Ames, IA")),
-               "type=dog&location=Ames,%20IA")
+                    "type=dog&location=Ames,%20IA")
 })
