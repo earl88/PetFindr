cd54bba2d67fbc174467bc2f57b9cfecb74aed0b
Amin Shirazi <ashirazi@iastate.edu>
2019-04-23 16:51:10 -0500

    test for breed update

--- tests/testthat/test-pf_list_breeds.R
+++ tests/testthat/test-pf_list_breeds.R
@@ -1,18 +1,13 @@
 context("test-pf_list_breeds")
 
-test_that("list breeds works", {
+test_that("list breeds has input as expected", {
   token <- readLines("token.txt")
   skip_if_not(exists("token"))
   expect_error(pf_list_breeds(token, type = 123))
-})
-
-test_that("the  type is a string ", {
-  token <- readLines("token.txt")
-  skip_if_not(exists("token"))
-  expect_true(is.character(pf_list_breeds(token, type = "dog")))
+  expect_error(pf_list_breeds(token, type = "unicorn"))
+  
   
 })
-
 
 test_that("the  output is a character vector", {
   token <- readLines("token.txt")
@@ -20,3 +15,4 @@ test_that("the  output is a character vector", {
   expect_true(is.character(pf_list_breeds(token, type = "dog")))
   
 })
+
