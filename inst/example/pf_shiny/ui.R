02123234904a57ecb1e94d9e2e4156feeaed04c1
Earl Hur <earlhur88@gmail.com>
2019-04-23 17:55:33 -0500

    pf_shiny folder

--- inst/example/pf_shiny/ui.R
+++ inst/example/pf_shiny/ui.R
@@ -28,6 +28,19 @@ shinyUI(navbarPage("PetFinder",
                             )
                           )
                  ),
-                 tabPanel("Location of shelters")
+                 tabPanel("Location of shelters",
+                          fluidRow(
+                            sidebarPanel(
+                              textInput("Zipcode", "Enter a 5-digit U.S. ZIP (postal) code to see the shelters:", value="50010"),
+                              selectInput("Distance", "Input miles from the zip code:", choices = seq(50, 500, by=50), selected = "100"),
+                              submitButton("Submit"),
+                              width = 4),
+                            mainPanel(
+                              tabsetPanel(
+                                tabPanel("Animal Shelters", leafletOutput("plot"),
+                                         width = 8)
+                              )
+                            )
+                          ))
                  )
 )
