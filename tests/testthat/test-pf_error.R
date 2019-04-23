06f31a311a62d8e229e1965087be318cb60cbd5e
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-23 15:24:05 -0500

    Test pf_error

--- tests/testthat/test-pf_error.R
+++ tests/testthat/test-pf_error.R
@@ -1,7 +1,12 @@
 context("test-pf_error")
 
 test_that("error codes function as expected", {
-  errors <- c(200, 400, 401)
-  msgs <- c("OK", "")
-  expect_equivalent(pf_error(200), "OK")
+  errors <- c(200, 400, 401, 403, 404, 500)
+  msgs <- c("OK", 
+            "The request is missing parameters or contains invalid parameters. For more information, go to https://www.petfinder.com/developers/v2/docs/.", 
+            "Access was denied due to invalid credentials. This could be an invalid API key/secret combination, missing access token, or expired access token.", 
+            "Access denied due to insufficient access.", 
+            "The requested resource was not found.", 
+            "The request ran into an unexpected error. If the problem persists, please contact support at https://www.petfinder.com/developers/support/.")
+  expect_equivalent(sapply(errors, pf_error), msgs)
 })
