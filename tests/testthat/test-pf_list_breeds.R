065c961cc532f5aab07f03ef183c21f5a2965d0f
Amin Shirazi <ashirazi@iastate.edu>
2019-04-23 17:24:49 -0500

    update tests for breed and types

--- tests/testthat/test-pf_list_breeds.R
+++ tests/testthat/test-pf_list_breeds.R
@@ -5,14 +5,9 @@ test_that("list breeds has input as expected", {
   skip_if_not(exists("token"))
   expect_error(pf_list_breeds(token, type = 123))
   expect_error(pf_list_breeds(token, type = "unicorn"))
+  expect_true(is.character(pf_list_breeds(token, type = "dog")))
   
   
 })
 
-test_that("the  output is a character vector", {
-  token <- readLines("token.txt")
-  skip_if_not(exists("token"))
-  expect_true(is.character(pf_list_breeds(token, type = "dog")))
-  
-})
 
--- tests/testthat/test-pf_list_types.R
+++ tests/testthat/test-pf_list_types.R
@@ -1,5 +1,21 @@
 context("test-pf_list_types")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("list breeds has input as expected", {
+  token <- readLines("token.txt")
+  skip_if_not(exists("token"))
+  expect_error(pf_list_types(token= 123))
+  expect_is(pf_list_types(token= token)[, 1], "data.frame")
+  expect_is(pf_list_types(token= token)[, 1], "tbl_df")
+  
+  
+})
+
+test_that("[ retains class", {
+  token <- readLines("token.txt")
+  skip_if_not(exists("token"))
+  mtcars2 <- pf_list_types(token)
+  
+  expect_identical(class(mtcars2), class(mtcars2[1:2, ]))
+  expect_identical(class(mtcars2), class(mtcars2[, 1:2]))
+  expect_identical(class(mtcars2), class(mtcars2[1:2, 1:2]))
 })
