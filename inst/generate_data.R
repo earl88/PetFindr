5cf956831446df6317483e86140f206ba9a8e390
jesslk <35576403+jesslk@users.noreply.github.com>
2019-04-24 16:53:52 -0500

    Sample Datasets

--- inst/generate_data.R
+++ inst/generate_data.R
@@ -7,5 +7,20 @@ pf_breeds <- pf_types %>%
   cbind(types$name, .) %>%
   setNames(c("name", "breeds"))
 
+
+
 # usethis::use_data(pf_types)
 # usethis::use_data(pf_breeds)
+
+
+#puppy.data
+LA_puppies <- pf_find_pets(token, type = "dog", age = "baby", location = 90405, 
+                           distance = 100, limit = 100, page = 1:5, sort = "recent")
+#usethis::use_data(LA_puppies)
+
+
+
+#organizations.data 
+NY_orgs <- pf_find_organizations(token, state = "NY", 
+                                 limit = 100, page = 1:5, sort = "-name")
+#usethis::use_data(NY_orgs)
