837fb5619bab4729521547a28e9ad8762eb83400
Earl Hur <earlhur88@gmail.com>
2019-04-24 17:33:03 -0500

    update

--- tests/testthat/test-pf_map_locations.R
+++ tests/testthat/test-pf_map_locations.R
@@ -3,6 +3,7 @@ token <- readLines("token.txt")
 
 test_that("data input behaves as expected", {
   skip_if_not(exists("token"))
+  expect_error(pf_map_locations(token))
   expect_error(pf_map_locations(token, data = 1))
   expect_error(pf_map_locations(token, data = "abc"))
 })
