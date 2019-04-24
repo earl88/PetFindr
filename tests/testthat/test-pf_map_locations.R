8ed2fa9762586e1d5dc1d0c9416cb2a48fa8321b
Earl Hur <earlhur88@gmail.com>
2019-04-24 17:04:31 -0500

    map_location test updated

--- tests/testthat/test-pf_map_locations.R
+++ tests/testthat/test-pf_map_locations.R
@@ -1,7 +1,7 @@
 context("test-pf_map_locations")
+token <- readLines("token.txt")
 
 test_that("data input behaves as expected", {
-  token <- readLines("token.txt")
   skip_if_not(exists("token"))
   expect_error(pf_map_locations(token, data = 1))
   expect_error(pf_map_locations(token, data = "abc"))
