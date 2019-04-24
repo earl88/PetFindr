835060408c48e764b8856d95f8383fb1f93a7ac0
Amin Shirazi <ashirazi@iastate.edu>
2019-04-24 16:44:49 -0500

    update test pf list type

--- tests/testthat/test-pf_list_types.R
+++ tests/testthat/test-pf_list_types.R
@@ -4,7 +4,8 @@ test_that("list breeds has input as expected", {
   token <- readLines("token.txt")
   skip_if_not(exists("token"))
   expect_error(pf_list_types(token= 123))
-  expect_true(tibble::is_tibble(pf_list_types(token= token)))
+  df<-pf_list_types(token= token)
+  expect_s3_class(df, "data.frame")
   expect_is(pf_list_types(token= token)[, 1], "data.frame")
   expect_is(pf_list_types(token= token)[, 1], "tbl_df")
   
