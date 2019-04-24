966948cb52978861cdf8beeb2da02eb771805d24
jesslk <35576403+jesslk@users.noreply.github.com>
2019-04-24 17:41:37 -0500

    Documentation

--- inst/generate_data.R
+++ inst/generate_data.R
@@ -15,12 +15,12 @@ pf_breeds <- pf_types %>%
 
 #puppy.data
 LA_puppies <- pf_find_pets(token, type = "dog", age = "baby", location = 90405, 
-                           distance = 100, limit = 100, page = 1:5, sort = "recent")
+                           distance = 100, limit = 100, page = 1:5, sort = "recent") %>% select("id", "organization_id", "type", "species", "age", "name")
 #usethis::use_data(LA_puppies)
 
 
 
 #organizations.data 
 NY_orgs <- pf_find_organizations(token, state = "NY", 
-                                 limit = 100, page = 1:5, sort = "-name")
+                                 limit = 100, page = 1:5, sort = "-name") %>% select("id", "name", "address.city", "address.state", "address.state", "address.postcode")
 #usethis::use_data(NY_orgs)
