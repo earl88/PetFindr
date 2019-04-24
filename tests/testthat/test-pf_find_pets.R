0d7df8bde29c83fc3a606d5a38152ec472b68b6c
Amin Shirazi <ashirazi@iastate.edu>
2019-04-24 17:42:56 -0500

    test_pf_find_pets is good to go

--- tests/testthat/test-pf_find_pets.R
+++ tests/testthat/test-pf_find_pets.R
@@ -1,5 +1,26 @@
 context("test-pf_find_pets")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+token <- readLines("token.txt")
+
+test_that("limit, sort and page inputs behaves as expected", {
+  
+  expect_error(pf_find_organizations(token, type= "dog", limit = -1))
+  expect_error(pf_find_pets(token, sort = "123"))
+  expect_error(pf_find_pets(token, page = 1000^1000))
+  
+ 
 })
+
+test_that("the output is a data frame", {
+  
+  skip_if_not(exists("token"))
+  df <- pf_find_pets(token = token, type = "dog")
+  expect_s3_class(df, "data.frame")
+
+})
+
+# test_that("output is a data frame  as expected", {
+#   skip_if_not(exists("token"))
+#   data(LA_puppies, package = "PetFindr")
+#   expect_true(is.data.frame(LA_puppies))
+# })
