59a3e35c9a83ceb24604fcd665b4e7566db0d937
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-24 22:05:08 -0500

    Mix merge conflict

--- inst/generate_data.R
+++ inst/generate_data.R
@@ -14,16 +14,13 @@ pf_breeds <- pf_types %>%
 
 
 #puppy.data
-LA_puppies <- pf_find_pets(token, type = "dog", age = "baby", location = 90405, 
-<<<<<<< HEAD
-                           distance = 100, limit = 100, page = 1:5, sort = "recent") 
+LA_puppies <- pf_find_pets(token, type = "dog", age = "baby", 
+                           location = 90405, distance = 100, limit = 100, 
+                           page = 1:5, sort = "recent") 
 rm_indices <- c(paste0("photos.[a-z]{4,6}.", 2:5), "hours", "tags", "links") %>%
   sapply(., grepl, names(LA_puppies)) %>% 
   apply(1, any)
-test <- LA_puppies[,!rm_indices]
-=======
-                           distance = 100, limit = 100, page = 1:5, sort = "recent") %>% select("id", "organization_id", "type", "species", "age", "name")
->>>>>>> 434fca3f45ba41d43cf5ce4f6bee73721520544c
+LA_puppies <- LA_puppies[,!rm_indices]
 #usethis::use_data(LA_puppies)
 
 
