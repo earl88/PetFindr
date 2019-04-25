be9015008fb223d8d1bcd18900c37ca5a9461501
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-25 10:32:49 -0500

    Add data files and documentation

--- data-raw/generate_data.R
+++ data-raw/generate_data.R
@@ -12,25 +12,25 @@ pf_breeds <- pf_types %>%
 
 
 # Animals example data
-LA_puppies <- pf_find_pets(token, type = "dog", age = "baby", 
-                           location = 90405, distance = 100, limit = 100, 
-                           page = 1:5, sort = "recent") 
+LA_puppies <- pf_find_pets(token, type = "dog", age = "baby",
+                           location = 90405, distance = 100, limit = 100,
+                           page = 1:5, sort = "recent")
 rm_indices <- c(paste0("photos.[a-z]{4,6}.", 2:5), "hours", "tags", "links") %>%
-  sapply(., grepl, names(LA_puppies)) %>% 
+  sapply(., grepl, names(LA_puppies)) %>%
   apply(1, any)
 keep_cols <- names(LA_puppies)[!rm_indices]
 LA_puppies <- LA_puppies[,keep_cols]
 
-#usethis::use_data(LA_puppies)
+# usethis::use_data(LA_puppies)
 
 
 
-# Organizations example data 
-NY_orgs <- pf_find_organizations(token, state = "NY", 
+# Organizations example data
+NY_orgs <- pf_find_organizations(token, state = "NY",
                                  limit = 100, page = 1:5, sort = "-name")
 rm_indices <- c(paste0("photos.[a-z]{4,6}.", 2:5), "hours", "tags", "links") %>%
-  sapply(., grepl, names(NY_orgs)) %>% 
+  sapply(., grepl, names(NY_orgs)) %>%
   apply(1, any)
 keep_cols <- names(NY_orgs)[!rm_indices]
 NY_orgs <- NY_orgs[,keep_cols]
-#usethis::use_data(NY_orgs)
+# usethis::use_data(NY_orgs)
