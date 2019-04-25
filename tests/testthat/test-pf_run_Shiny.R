08047f463e05e0b808ceaa32ff15a4a8422a290b
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-24 23:54:13 -0500

    Fix shiny tests to not fail on Travis (I hope)

--- tests/testthat/test-pf_run_Shiny.R
+++ tests/testthat/test-pf_run_Shiny.R
@@ -1,7 +1,8 @@
 context("test-pf_run_Shiny")
 
-# test_that("file exist", {
-#   expect_true(file.exists(file.path(system.file("shiny-example\\pf_shiny", package = "PetFindr"), "global.R")))
-#   expect_true(file.exists(file.path(system.file("shiny-example\\pf_shiny", package = "PetFindr"), "server.R")))
-#   expect_true(file.exists(file.path(system.file("shiny-example\\pf_shiny", package = "PetFindr"), "ui.R")))
-# })
+test_that("file exist", {
+  skip_if_not(dir.exists("../../inst/shiny-example"))
+  expect_true(file.exists(file.path(system.file("shiny-example\\pf_shiny", package = "PetFindr"), "global.R")))
+  expect_true(file.exists(file.path(system.file("shiny-example\\pf_shiny", package = "PetFindr"), "server.R")))
+  expect_true(file.exists(file.path(system.file("shiny-example\\pf_shiny", package = "PetFindr"), "ui.R")))
+})
