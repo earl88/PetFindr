6f49205ecebd53587b18ae427e01210ea729f7a9
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-24 23:34:10 -0500

    Try to implement view_photos tests with live search

--- tests/testthat/test-pf_view_photos.R
+++ tests/testthat/test-pf_view_photos.R
@@ -1,15 +1,14 @@
 context("test-pf_view_photos")
 
 test_that("correct animal dataframe ", {
-
-  # expect an error due to incorrect search result from pf_find_pet
   expect_error(pf_view_photos(animal_df = puppies, size = 123))
-  
-  # expect an error due to incorrect size
   expect_error(pf_view_photos(animal_df = puppies, size = "abc"))
   
-  
-  
+  token <- readLines("token.txt")
+  skip_if_not(exists("token"))
+  df <- pf_find_pets(token, type = "dog", limit = 3)
+  output <- pf_view_photos(df, "small")
+  expect_true(tibble::is_tibble(magick::image_info(output)))
 })
 
 # test_that("photo view has output as expected", {
