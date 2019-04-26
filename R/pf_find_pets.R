3d1b351c4054fa0c20b87b39630e49cc16234d13
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-26 13:32:42 -0500

    Handle blank inputs

--- R/pf_find_pets.R
+++ R/pf_find_pets.R
@@ -40,6 +40,7 @@ pf_find_pets <- function(token = NULL, type = NULL, breed = NULL, size = NULL,
   
   args <- as.list(match.call())[-1]
   query_args <- args[!names(args) %in% c("token", "page")] %>% purrr::map(eval)
+  query_args <- query_args[query_args != ""]
   
   if(length(query_args) > 0) {
     query_args <- stats::setNames(unlist(query_args, use.names=F),
