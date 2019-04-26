a2e718d33d41dff495a836cf64b689137c6fb966
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-26 17:39:18 -0500

    Fix view_photos with error checking

--- R/pf_view_photos.R
+++ R/pf_view_photos.R
@@ -21,30 +21,34 @@
 #' }
 pf_view_photos <- function(animal_df, 
                            size = c("small", "medium", "large", "full")) {
-    assertthat::assert_that(is.data.frame(animal_df))
-    assertthat::not_empty(animal_df)
-    size <- match.arg(size)
-    
-    animal_photos <- animal_df %>%
-      dplyr::select(tidyselect::vars_select(
-        names(animal_df),
-        dplyr::starts_with(paste0("photos.", size), ignore.case = TRUE)))
-    
-    assertthat::not_empty(animal_photos)
-    animal_photos <- animal_photos %>%
-      lapply(FUN = as.character) %>%
-      purrr::flatten()
-    
-    npix <- (as.character(c(100, 300, 600, 600)) %>%
-               stats::setNames(c("small", "medium", "large", "full")))[size]
-    
-    photos <- purrr::map_chr(animal_photos, magrittr::extract2, 1) %>%
-      stats::na.omit() %>%
-      stringr::str_remove(paste0("&width=", npix))
-    
-    return(photos %>%
-             knitr::include_graphics() %>%
-             magick::image_read() %>%
-             magick::image_scale("x400")
-    )
+  assertthat::assert_that(is.data.frame(animal_df))
+  assertthat::not_empty(animal_df)
+  size <- match.arg(size)
+  
+  animal_photos <- animal_df %>%
+    dplyr::select(tidyselect::vars_select(names(animal_df),
+                  dplyr::starts_with(paste0("photos.", size), 
+                                     ignore.case = TRUE)))
+  
+  assertthat::not_empty(animal_photos)
+  animal_photos <- as.vector(sapply(animal_photos, as.character))
+  if(all(is.na(animal_photos))) {
+    stop("No photos found :(")
   }
+  
+  npix <- (as.character(c(100, 300, 600, 600)) %>%
+             stats::setNames(c("small", "medium", "large", "full")))[size]
+  
+  photos <- animal_photos %>%
+    stats::na.omit() %>%
+    stringr::str_remove(paste0("&width=", npix))
+  # if(length(photos) == 0) {
+  #   stop("No photos found!!! :(")
+  # }
+  
+  return(photos %>%
+           knitr::include_graphics() %>%
+           magick::image_read() %>%
+           magick::image_scale("x400")
+  )
+}
