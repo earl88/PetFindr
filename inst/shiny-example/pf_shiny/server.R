840f89d603c1ccb8c4fccde5c0425a8d4c92b6ce
Earl Hur <earlhur88@gmail.com>
2019-04-28 16:54:28 -0500

    shinyapp updated with logo inserted in www folder.

--- inst/shiny-example/pf_shiny/server.R
+++ inst/shiny-example/pf_shiny/server.R
@@ -1,5 +1,13 @@
 function(input, output, session) {
   
+  url <- a("Petfinder API", href="https://www.petfinder.com/developers/")
+  
+  observeEvent(input$setup, {
+    output$instruction <- renderUI(
+      tagList("URL link:", url)
+      )
+  },ignoreInit = TRUE)
+  
   if (exists("petfindr_key")) {
     updateTextInput(session, "key", value = petfindr_key)
   }
