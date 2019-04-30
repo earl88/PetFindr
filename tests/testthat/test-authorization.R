0c0349525193a79be4822b0e295087cde7251281
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-30 15:31:39 -0500

    Make tests clearer

--- tests/testthat/test-authorization.R
+++ tests/testthat/test-authorization.R
@@ -9,27 +9,30 @@ test_that("can use key and secret", {
   skip_if_not(exists(c("test_key", "test_secret")))
   token <- expect_message(pf_accesstoken(test_key, test_secret), 
                           "Your access token will last for one hour")
+  
   if (nchar(token) > 0) writeLines(token, "token.txt")
-  expect_error(pf_accesstoken(key = "ceci n'est pas une key"))
-  expect_error(pf_accesstoken(key = "ceci n'est pas une key",
-                              secret = "ceci n'est pas une secret"))
+  expect_error(pf_accesstoken(key = "Ceci n'est pas une key"))
+  expect_error(pf_accesstoken(key = "Ceci n'est pas une key",
+                              secret = "Ceci n'est pas une secret"))
 })
 
 test_that("can save credentials", {
-  if(!file.exists(".RProfile")) writeLines("# Test\n", ".Rprofile")
-  skip_if_not(file.exists(".Rprofile"))
-
+  # No .Rprofile should exist yet
   expect_error(pf_save_credentials())
   
+  if(!file.exists(".RProfile")) writeLines("# Test\n", ".Rprofile")
+  skip_if_not(file.exists(".Rprofile"))
+  expect_error(pf_save_credentials())
+  
+  # Check key inputs and errors
   expect_error(pf_save_credentials(key = 1))
   expect_error(pf_save_credentials(key = "Ceci n'est pas une key"))
-  
   pf_save_credentials(key = "Ceci n'est pas une key, mais il a 50 caracteres :)")
   expect_warning(pf_save_credentials(key = "This is not a key but it also has 50 characters :)"))
   
+  # Check secret inputs and errors
   expect_error(pf_save_credentials(secret = 1))
   expect_error(pf_save_credentials(secret = "Ceci n'est pas une secret"))
-  
   pf_save_credentials(secret = "Ceci n'est pas une secret, c'est vrai :(")
   expect_warning(pf_save_credentials(secret = "This is not a secret; so sad but true :("))
 })
